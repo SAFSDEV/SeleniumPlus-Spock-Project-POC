4e33893fbf62991e5cbc010e1a803b650e41baf4
sascanagl <carl.nagle@sas.com>
2016-05-18 02:14:37 +0100

    add Java assertion test into the mix
    added a Java assertion test into the mix to evaluate how we will
    properly report test failures in JUnit tests.

--- src/com/sas/spock/tests/GeneralJUnitTest.java
+++ src/com/sas/spock/tests/GeneralJUnitTest.java
@@ -18,14 +18,28 @@ public class GeneralJUnitTest {
 	}
 	
 	@Test
-	public void test1() {
+	public void selenium_Assert_pass() {
 		Assert.Equals(a, b);
 	}
 	
 	@Test
-	public void test2() {
+	public void selenium_Assert_fail() {
 		//This test will fail
 		Assert.Equals(a, c);
+	}
+	
+	/**
+	 * This test using Java assert will only fail if assertions are enabled for this class:
+	 * <p>
+	 * requires JVM arg: -ea:com.sas.spock.tests.GeneralJUnitTest
+	 * <p>
+	 * Otherwise, with Java assertions disabled by default,this assert test never actually happens at all.
+	 */
+	@Test
+	public void java_assert_fail() {
+		//This test will fail if assertions are enabled
+		// JVM command-line arg: -ea:com.sas.spock.tests.GeneralJUnitTest
+		assert a==c:"compare a==c failed";
 	}
 	
 	@After
